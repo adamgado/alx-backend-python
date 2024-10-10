0fe76eaf0db10928ebd59e07d83e6b20a7e8067d
adamgado <adamgado33@gmail.com>
2024-10-10 12:05:14 +0000

    1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""function takes two string arguments and returns a concatenated string"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """return a concatenated string"""
+    return str1 + str2
