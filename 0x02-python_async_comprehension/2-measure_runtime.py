762ae3ca507388940840290727826ced7318ca79
adamgado <adamgado33@gmail.com>
2024-10-15 15:21:55 +0000

    2

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""coroutine that will execute async_comprehension four times in parallel"""
+import asyncio
+import random
+from typing import List
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_generator
+
+
+async def measure_runtime() -> float:
+    """measure the total runtime and return it"""
+    start = time.time()
+    await asyncio.gather(*(async_comprehension() for a in range(4)))
+    return time.time() - start
